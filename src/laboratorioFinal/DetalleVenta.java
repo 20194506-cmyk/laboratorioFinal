24577fa9bebf14f6a3f19736b6335e78d03e9cf5
rafa <paularafael892@gmail.com>
2026-06-15 15:24:55 -0600

    Cambios

--- src/laboratorioFinal/DetalleVenta.java
+++ src/laboratorioFinal/DetalleVenta.java
@@ -2,4 +2,26 @@ package laboratorioFinal;
 
 public class DetalleVenta {
 
+    private int idDetalle;
+    private int cantidad;
+    private Producto producto;
+    private double subtotal;
+
+    public DetalleVenta(int idDetalle, int cantidad, Producto producto) {
+        this.idDetalle = idDetalle;
+        this.cantidad = cantidad;
+        this.producto = producto;
+        this.subtotal = cantidad * producto.getPrecioVenta();
+    }
+
+    public double getSubtotal() {
+        return subtotal;
+    }
+
+    @Override
+    public String toString() {
+        return producto.getNombre() +
+                " x" + cantidad +
+                " = $" + subtotal;
+    }
 }
--- src/laboratorioFinal/Venta.java
+++ src/laboratorioFinal/Venta.java
@@ -1,5 +1,60 @@
 package laboratorioFinal;
 
+import java.util.ArrayList;
+
 public class Venta {
 
+    private int idVenta;
+    private String fecha;
+    private Cliente cliente;
+    private Empleado empleado;
+    private ArrayList<DetalleVenta> detalles;
+
+    public Venta(int idVenta, String fecha,
+                 Cliente cliente,
+                 Empleado empleado) {
+
+        this.idVenta = idVenta;
+        this.fecha = fecha;
+        this.cliente = cliente;
+        this.empleado = empleado;
+
+        detalles = new ArrayList<>();
+    }
+
+    public void agregarDetalle(DetalleVenta detalle) {
+        detalles.add(detalle);
+    }
+
+    public double calcularTotal() {
+
+        double total = 0;
+
+        for (DetalleVenta detalle : detalles) {
+            total += detalle.getSubtotal();
+        }
+
+        return total;
+    }
+
+    @Override
+    public String toString() {
+
+        String texto = "";
+
+        texto += "VENTA #" + idVenta + "\n";
+        texto += "Fecha: " + fecha + "\n";
+        texto += "Cliente: " + cliente.getNombre() + "\n";
+        texto += "Empleado: " + empleado.getNombre() + "\n\n";
+
+        texto += "DETALLES:\n";
+
+        for (DetalleVenta detalle : detalles) {
+            texto += detalle + "\n";
+        }
+
+        texto += "\nTOTAL: $" + calcularTotal();
+
+        return texto;
+    }
 }
