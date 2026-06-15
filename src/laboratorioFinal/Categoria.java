94586de1e9fdb73cd088c3d346933e348516e97e
Jose <jf3034020@gmail.com>
2026-06-15 15:34:34 -0600

    Agregar archivos de Categoria y Produnto

--- src/laboratorioFinal/Categoria.java
+++ src/laboratorioFinal/Categoria.java
@@ -1,5 +1,26 @@
 package laboratorioFinal;
 
 public class Categoria {
+	
+	    private int idCategoria;
+	    private String nombre;
 
-}
+	    public Categoria(int idCategoria, String nombre) {
+	        this.idCategoria = idCategoria;
+	        this.nombre = nombre;
+	    }
+
+	    public int getIdCategoria() {
+	        return idCategoria;
+	    }
+
+	    public String getNombre() {
+	        return nombre;
+	    }
+
+	    @Override
+	    public String toString() {
+	        return nombre;
+	    }
+	}
+
--- src/laboratorioFinal/Producto.java
+++ src/laboratorioFinal/Producto.java
@@ -2,4 +2,47 @@ package laboratorioFinal;
 
 public class Producto {
 
+    private int idProducto;
+    private String nombre;
+    private double precioCompra;
+    private double precioVenta;
+    private Categoria categoria;
+
+    public Producto(int idProducto,
+                    String nombre,
+                    double precioCompra,
+                    double precioVenta,
+                    Categoria categoria) {
+
+        this.idProducto = idProducto;
+        this.nombre = nombre;
+        this.precioCompra = precioCompra;
+        this.precioVenta = precioVenta;
+        this.categoria = categoria;
+    }
+
+    public int getIdProducto() {
+        return idProducto;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public double getPrecioCompra() {
+        return precioCompra;
+    }
+
+    public double getPrecioVenta() {
+        return precioVenta;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    @Override
+    public String toString() {
+        return nombre + " - $" + precioVenta;
+    }
 }
